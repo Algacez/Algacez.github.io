14f323d604586e105aa8bf09bc094ecc943429dc
algacez <lyyousaforever@gmail.com>
2025-08-21 22:29:10 +0800

    refactor(site): 更新网站配置和内容
    - 在 package.json 中添加 @volar/kit 依赖
    - 更新 pnpm-lock.yaml 中的依赖版本

--- src/config.ts
+++ src/config.ts
@@ -40,22 +40,22 @@ export const siteConfig: SiteConfig = {
 		// Support single image or image array, carousel is automatically enabled when array length > 1
 		src: {
 			desktop: [
-				"assets/desktop-banner/1.webp",
-				"assets/desktop-banner/2.webp",
-				"assets/desktop-banner/3.webp",
-				"assets/desktop-banner/4.webp",
-				"assets/desktop-banner/5.webp",
-				"assets/desktop-banner/6.webp",
-				"assets/desktop-banner/7.webp",
+				//	"assets/desktop-banner/1.webp",
+				//	"assets/desktop-banner/2.webp",
+				//	"assets/desktop-banner/3.webp",
+				//	"assets/desktop-banner/4.webp",
+				//	"assets/desktop-banner/5.webp",
+				//	"assets/desktop-banner/6.webp",
+				//	"assets/desktop-banner/7.webp",
 			], // Desktop banner images
 			mobile: [
-				"assets/mobile-banner/1.webp",
-				"assets/mobile-banner/2.webp",
-				"assets/mobile-banner/3.webp",
-				"assets/mobile-banner/4.webp",
-				"assets/mobile-banner/5.webp",
-				"assets/mobile-banner/6.webp",
-				"assets/mobile-banner/7.webp",
+				//	"assets/mobile-banner/1.webp",
+				//	"assets/mobile-banner/2.webp",
+				//	"assets/mobile-banner/3.webp",
+				//	"assets/mobile-banner/4.webp",
+				//	"assets/mobile-banner/5.webp",
+				//	"assets/mobile-banner/6.webp",
+				//	"assets/mobile-banner/7.webp",
 			], // Mobile banner images
 		}, // Use local banner images
 
